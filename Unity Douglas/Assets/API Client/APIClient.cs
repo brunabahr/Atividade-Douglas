3e7d93219e16ed27c8e604827422f6dd170ba8ca
Bruna Bahr <bruninhabdc@hotmail.com>
2017-11-01 22:33:50 -0200

    Unity2

---
+++ Unity Douglas/Assets/API Client/APIClient.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class APIClient : MonoBehaviour
+{
+    [HideInInspector] public Modelo3D[] modelos = null;
+    private const string baseUrl = "http://localhost:55982/API";
+
+	void Start ()
+    {
+        StartCoroutine("GetItensAPISync");
+	}
+
+    IEnumerator GetItensAPISync()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(baseUrl + "/Modelos3D");
+
+        yield return request.Send();
+
+        if(request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            string response = request.downloadHandler.text;
+            Debug.Log(response);
+
+            Modelo3D[] modelos = JSonHelper.getJsonArray<Modelo3D>(response);
+
+            foreach (Modelo3D i in modelos)
+            {
+                ImprimirItem(i);
+            }
+        }
+    }
+
+    private void ImprimirItem(Modelo3D i)
+    {
+        Debug.Log("=========== Dados do Objeto ===========");
+        Debug.Log("ID: " + i.Modelo3DID);
+        Debug.Log("Nome: " + i.Nome);
+        Debug.Log("Idade: " + i.Idade);
+        Debug.Log("Altura: " + i.Altura);
+        Debug.Log("Peso: " + i.Peso);
+        Debug.Log("Historia: " + i.Historia);
+        Debug.Log("Habilidade: " + i.Habilidade);
+            
+    }
+}
---
+++ Unity Douglas/Assets/Models/Modelo3D.cs
@@ -0,0 +1,12 @@
+﻿[System.Serializable]
+public class Modelo3D
+{
+    public int Modelo3DID;
+    public string Nome;
+    public int Idade;
+    public int Altura;
+    public int Peso;
+    public string Historia;
+    public string Habilidade;
+
+}
