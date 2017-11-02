010980cac456b5d438ba640230f9097a3bb08594
Bruna Bahr <bruninhabdc@hotmail.com>
2017-11-01 22:08:15 -0200

    Unity

---
+++ Unity Douglas/Assets/APIClient.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class APIClient : MonoBehaviour
+{
+    [HideInInspector] public Item[] itens = null;
+    private const string baseUrl = "http://localhost:54853/API";
+
+	void Start ()
+    {
+        StartCoroutine("GetItensAPISync");
+        StartCoroutine("PostItemApiSync");
+	}
+
+    IEnumerator GetItensAPISync()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(baseUrl + "/Itens");
+
+        yield return request.Send();
+
+        if(request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            string response = request.downloadHandler.text;
+            Debug.Log(response);
+
+            itens = JSonHelper.getJsonArray<Item>(response);
+
+            foreach (Item i in itens)
+            {
+                ImprimirItem(i);
+            }
+        }
+    }
+
+    IEnumerator PostItemApiSync()
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("Nome", "ItemFromUnity");
+        form.AddField("Descrição", "Item enviado por POST para Unity3D");
+        form.AddField("DanoMaximo", "5");
+        form.AddField("Raridade", "90");
+        form.AddField("TipoItemID", "2");
+
+        using (UnityWebRequest request = UnityWebRequest.Post(baseUrl + "/Itens/Create", form))
+        {
+            yield return request.Send();
+            //yield return request.SendWebRequest(); Unity 2017.2  52.219
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                Debug.Log("Envio do item efetudado com sucesso");
+            }
+        }
+    }
+
+    private void ImprimirItem(Item i)
+    {
+        Debug.Log("=========== Dados do Objeto ===========");
+        Debug.Log("ID: " + i.ItemID);
+        Debug.Log("Nome: " + i.Nome);
+        Debug.Log("Descrição: " + i.Descricao);
+        Debug.Log("Dano Máximo: " + i.DanoMaximo);
+        Debug.Log("Raridade: " + i.Raridade);
+        Debug.Log("Tipo Item ID: " + i.Raridade);
+    }
+}
---
+++ Unity Douglas/Assets/JSonHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSonHelper
+{
+    public static T[] getJsonArray<T>(string json)
+    {
+        string newJson = "{\"array\":" + json + "}";
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+
+        return wrapper.array;
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+}
